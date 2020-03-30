7082722e3aeaa10b1e91e82f713b446248b9f763
Braun <looseymoose@FT106-106-M2.local>
2020-03-30 08:18:33 -0400

    Switch to postgres db

--- cafe/cafe/settings.py
+++ cafe/cafe/settings.py
@@ -88,18 +88,18 @@ WSGI_APPLICATION = 'cafe.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'cafe',
-    #     'USER': 'cafeuser',
-    #     'PASSWORD': 'inmelet1',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '',
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cafe',
+        'USER': 'cafeuser',
+        'PASSWORD': 'inmelet1',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    }
 }
 
 
